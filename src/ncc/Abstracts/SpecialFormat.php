5d6200608366ec8b5af86f9345578662fd90bb27
Netkas <zixing.narrakas1996@gmail.com>
2022-11-01 18:29:45 -0400

    Added comments to SpecialFormat.php

--- src/ncc/Abstracts/SpecialFormat.php
+++ src/ncc/Abstracts/SpecialFormat.php
@@ -4,29 +4,68 @@
 
     abstract class SpecialFormat
     {
+        /**
+         * Assembly's Name Property
+         */
         const AssemblyName = '%ASSEMBLY.NAME%';
 
+        /**
+         * Assembly's Package Property
+         */
         const AssemblyPackage = '%ASSEMBLY.PACKAGE%';
 
+        /**
+         * Assembly's Description Property
+         */
         const AssemblyDescription = '%ASSEMBLY.DESCRIPTION%';
 
+        /**
+         * Assembly's Company Property
+         */
         const AssemblyCompany = '%ASSEMBLY.COMPANY%';
 
+        /**
+         * Assembly's Product Property
+         */
         const AssemblyProduct = '%ASSEMBLY.PRODUCT%';
 
+        /**
+         * Assembly's Copyright Property
+         */
         const AssemblyCopyright = '%ASSEMBLY.COPYRIGHT%';
 
+        /**
+         * Assembly's Trademark Property
+         */
         const AssemblyTrademark = '%ASSEMBLY.TRADEMARK%';
 
+        /**
+         * Assembly's Version Property
+         */
         const AssemblyVersion = '%ASSEMBLY.VERSION%';
 
+        /**
+         * Assembly's UUID property
+         */
         const AssemblyUid = '%ASSEMBLY.UID%';
 
+        /**
+         * The Unix Timestamp for when the package was compield
+         */
         const CompileTimestamp = '%COMPILE_TIMESTAMP%';
 
+        /**
+         * The version of NCC that was used to compile the package
+         */
         const NccBuildVersion = '%NCC_BUILD_VERSION%';
 
-        const NccBuildArgs = '%NCC_BUILD_FLAGS%';
+        /**
+         * NCC Build Flags exploded into spaces
+         */
+        const NccBuildFlags = '%NCC_BUILD_FLAGS%';
 
+        /**
+         * NCC Build Branch
+         */
         const NccBuildBranch = '%NCC_BUILD_BRANCH%';
     }
