265d42d75de2313d7672b1485b3ecf45610aee63
Netkas <zixing.narrakas1996@gmail.com>
2022-11-06 14:34:56 -0500

    Added method fread() to \ncc\Utilities > IO

--- src/ncc/Utilities/IO.php
+++ src/ncc/Utilities/IO.php
@@ -2,6 +2,8 @@
 
     namespace ncc\Utilities;
 
+    use ncc\Exceptions\AccessDeniedException;
+    use ncc\Exceptions\FileNotFoundException;
     use ncc\Exceptions\IOException;
     use SplFileInfo;
     use SplFileObject;
@@ -22,7 +24,7 @@
         {
             $fileInfo = new SplFileInfo($uri);
 
-            if(!is_dir($fileInfo->getPath()))
+            if(is_dir($fileInfo->getPath()))
             {
                 throw new IOException(sprintf('Attempted to write data to a directory instead of a file: (%s)', $uri));
             }
@@ -45,5 +47,46 @@
             {
                 throw new IOException(sprintf('Unable to chmod: (%s) to (%s)', $uri, $perms));
             }
+        }
+
+        /**
+         * Attempts to read the specified file
+         *
+         * @param string $uri
+         * @param string $mode
+         * @param int|null $length
+         * @return string
+         * @throws AccessDeniedException
+         * @throws FileNotFoundException
+         * @throws IOException
+         */
+        public static function fread(string $uri, string $mode='r', ?int $length=null): string
+        {
+            $fileInfo = new SplFileInfo($uri);
+
+            if(is_dir($fileInfo->getPath()))
+            {
+                throw new IOException(sprintf('Attempted to read data from a directory instead of a file: (%s)', $uri));
+            }
+
+            if(!file_exists($uri))
+            {
+                throw new FileNotFoundException(sprintf('Cannot find file %s', $uri));
+            }
+
+            if(!is_readable($uri))
+            {
+                throw new AccessDeniedException(sprintf('Insufficient permissions to read %s', $uri));
+            }
+
+            $file = new SplFileObject($uri, $mode);
+            if($length == null)
+            {
+                $length = $file->getSize();
+                if(!$length)
+                    throw new IOException(sprintf('Cannot determine file size for %s', $uri));
+            }
+            return $file->fread($length);
+
         }
     }
