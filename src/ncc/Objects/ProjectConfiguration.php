eae1ea7b25ca083e40869cf2777d9f51a5cf4a31
Netkas <zixing.narrakas1996@gmail.com>
2022-11-11 04:10:20 -0500

    Implemented Execution Policy reference validation in \ncc\Objects > ProjectConfiguration
    added method getRequiredExecutionPolicies() to \ncc\Objects > ProjectConfiguration to return required execution policies for the selected build configuration
    
    Added method processBuildPolicies() to \ncc\Objects > ProjectConfiguration to return required execution policies for the given build configuration

--- src/ncc/Objects/ProjectConfiguration.php
+++ src/ncc/Objects/ProjectConfiguration.php
@@ -4,18 +4,26 @@
 
     namespace ncc\Objects;
 
+    use Exception;
+    use ncc\Abstracts\Options\BuildConfigurationValues;
+    use ncc\Exceptions\AccessDeniedException;
+    use ncc\Exceptions\BuildConfigurationNotFoundException;
     use ncc\Exceptions\FileNotFoundException;
     use ncc\Exceptions\InvalidConstantNameException;
     use ncc\Exceptions\InvalidProjectBuildConfiguration;
     use ncc\Exceptions\InvalidProjectConfigurationException;
     use ncc\Exceptions\InvalidPropertyValueException;
+    use ncc\Exceptions\IOException;
     use ncc\Exceptions\MalformedJsonException;
     use ncc\Exceptions\RuntimeException;
+    use ncc\Exceptions\UndefinedExecutionPolicyException;
     use ncc\Exceptions\UnsupportedCompilerExtensionException;
     use ncc\Exceptions\UnsupportedExtensionVersionException;
     use ncc\Objects\ProjectConfiguration\Assembly;
     use ncc\Objects\ProjectConfiguration\Build;
+    use ncc\Objects\ProjectConfiguration\BuildConfiguration;
     use ncc\Objects\ProjectConfiguration\ExecutionPolicy;
+    use ncc\Objects\ProjectConfiguration\Installer;
     use ncc\Objects\ProjectConfiguration\Project;
     use ncc\Utilities\Functions;
 
@@ -47,6 +55,13 @@
         public $ExecutionPolicies;
 
         /**
+         * Execution Policies to execute by the NCC installer
+         *
+         * @var Installer|null
+         */
+        public $Installer;
+
+        /**
          * Build configuration for the project
          *
          * @var Build
@@ -69,13 +84,15 @@
          *
          * @param bool $throw_exception
          * @return bool
+         * @throws BuildConfigurationNotFoundException
+         * @throws InvalidConstantNameException
+         * @throws InvalidProjectBuildConfiguration
          * @throws InvalidProjectConfigurationException
          * @throws InvalidPropertyValueException
          * @throws RuntimeException
+         * @throws UndefinedExecutionPolicyException
          * @throws UnsupportedCompilerExtensionException
          * @throws UnsupportedExtensionVersionException
-         * @throws InvalidProjectBuildConfiguration
-         * @throws InvalidConstantNameException
          */
         public function validate(bool $throw_exception=True): bool
         {
@@ -88,7 +105,110 @@
             if(!$this->Build->validate($throw_exception))
                 return false;
 
+            try
+            {
+                $this->getRequiredExecutionPolicies(BuildConfigurationValues::AllConfigurations);
+            }
+            catch(Exception $e)
+            {
+                if($throw_exception)
+                    throw $e;
+                return false;
+            }
+
             return true;
+        }
+
+        /**
+         * Runs a check on the project configuration and determines what policies are required
+         *
+         * @param string $build_configuration
+         * @return array
+         * @throws BuildConfigurationNotFoundException
+         * @throws UndefinedExecutionPolicyException
+         */
+        public function getRequiredExecutionPolicies(string $build_configuration=BuildConfigurationValues::DefaultConfiguration): array
+        {
+            if($this->ExecutionPolicies == null || count($this->ExecutionPolicies) == 0)
+                return [];
+
+            $defined_polices = [];
+            $required_policies = [];
+            /** @var ExecutionPolicy $execution_policy */
+            foreach($this->ExecutionPolicies as $execution_policy)
+            {
+                $defined_polices[] = $execution_policy->Name;
+                $execution_policy->validate();
+            }
+
+            // Check the installer by batch
+            if($this->Installer !== null)
+            {
+                $array_rep = $this->Installer->toArray();
+                /** @var string[] $value */
+                foreach($array_rep as $key => $value)
+                {
+                    if($value == null || count($value) == 0)
+                        continue;
+
+                    foreach($value as $unit)
+                    {
+                        if(!in_array($unit, $defined_polices))
+                            throw new UndefinedExecutionPolicyException('The property \'' . $key . '\' in the project configuration calls for an undefined execution policy \'' . $unit . '\'');
+                        if(!in_array($unit, $required_policies))
+                            $required_policies[] = $unit;
+                    }
+                }
+            }
+
+            if($this->Build->PreBuild !== null && count($this->Build->PostBuild) > 0)
+            {
+                foreach($this->Build->PostBuild as $unit)
+                {
+                    if(!in_array($unit, $defined_polices))
+                        throw new UndefinedExecutionPolicyException('The property \'build.pre_build\' in the project configuration calls for an undefined execution policy \'' . $unit . '\'');
+                    if(!in_array($unit, $required_policies))
+                        $required_policies[] = $unit;
+                }
+            }
+
+            if($this->Build->PostBuild !== null && count($this->Build->PostBuild) > 0)
+            {
+                foreach($this->Build->PostBuild as $unit)
+                {
+                    if(!in_array($unit, $defined_polices))
+                        throw new UndefinedExecutionPolicyException('The property \'build.pre_build\' in the project configuration calls for an undefined execution policy \'' . $unit . '\'');
+                    if(!in_array($unit, $required_policies))
+                        $required_policies[] = $unit;
+                }
+            }
+
+            switch($build_configuration)
+            {
+                case BuildConfigurationValues::AllConfigurations:
+                    /** @var BuildConfiguration $configuration */
+                    foreach($this->Build->Configurations as $configuration)
+                    {
+                        foreach($this->processBuildPolicies($configuration, $defined_polices) as $policy)
+                        {
+                            if(!in_array($policy, $required_policies))
+                                $required_policies[] = $policy;
+                        }
+                    }
+                    break;
+
+                default:
+                    $configuration = $this->Build->getBuildConfiguration($build_configuration);
+                    foreach($this->processBuildPolicies($configuration, $defined_polices) as $policy)
+                    {
+                        if(!in_array($policy, $required_policies))
+                            $required_policies[] = $policy;
+                    }
+                    break;
+            }
+
+
+            return $required_policies;
         }
 
         /**
@@ -147,6 +267,10 @@
             $ProjectConfigurationObject->Assembly = Assembly::fromArray(Functions::array_bc($data, 'assembly'));
             $ProjectConfigurationObject->ExecutionPolicies = Functions::array_bc($data, 'execution_policies');
             $ProjectConfigurationObject->Build = Build::fromArray(Functions::array_bc($data, 'build'));
+            $ProjectConfigurationObject->Installer = Functions::array_bc($data, 'installer');
+
+            if($ProjectConfigurationObject->Installer !== null)
+                $ProjectConfigurationObject->Installer = Installer::fromArray($ProjectConfigurationObject->Installer);
 
             if($ProjectConfigurationObject->ExecutionPolicies == null)
             {
@@ -172,10 +296,45 @@
          * @return ProjectConfiguration
          * @throws FileNotFoundException
          * @throws MalformedJsonException
+         * @throws AccessDeniedException
+         * @throws IOException
          * @noinspection PhpUnused
          */
         public static function fromFile(string $path): ProjectConfiguration
         {
             return ProjectConfiguration::fromArray(Functions::loadJsonFile($path, Functions::FORCE_ARRAY));
+        }
+
+        /**
+         * @param BuildConfiguration $configuration
+         * @param array $defined_polices
+         * @return array
+         * @throws UndefinedExecutionPolicyException
+         */
+        private function processBuildPolicies(BuildConfiguration $configuration, array $defined_polices): array
+        {
+            $required_policies = [];
+
+            if ($configuration->PreBuild !== null && count($configuration->PreBuild) > 0)
+            {
+                foreach ($configuration->PreBuild as $unit)
+                {
+                    if (!in_array($unit, $defined_polices))
+                        throw new UndefinedExecutionPolicyException('The property \'pre_build\' in the build configuration \'' . $configuration->Name . '\' calls for an undefined execution policy \'' . $unit . '\'');
+                    $required_policies[] = $unit;
+                }
+            }
+
+            if ($configuration->PostBuild !== null && count($configuration->PostBuild) > 0)
+            {
+                foreach ($configuration->PostBuild as $unit)
+                {
+                    if (!in_array($unit, $defined_polices))
+                        throw new UndefinedExecutionPolicyException('The property \'pre_build\' in the build configuration \'' . $configuration->Name . '\' calls for an undefined execution policy \'' . $unit . '\'');
+                    $required_policies[] = $unit;
+                }
+            }
+
+            return $required_policies;
         }
     }
