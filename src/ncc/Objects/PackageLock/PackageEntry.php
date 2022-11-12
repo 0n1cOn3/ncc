f7a1b6ed1691b2edfe6ec145c50ad5f6dcc7534f
Netkas <zixing.narrakas1996@gmail.com>
2022-11-12 06:51:43 -0500

    Added methods removeVersion(), getLatestVersion(), getVersions() to \ncc\Objects\PackageLock > PackageEntry

--- src/ncc/Objects/PackageLock/PackageEntry.php
+++ src/ncc/Objects/PackageLock/PackageEntry.php
@@ -5,6 +5,8 @@
     namespace ncc\Objects\PackageLock;
 
     use ncc\Exceptions\VersionNotFoundException;
+    use ncc\Objects\Package;
+    use ncc\ThirdParty\jelix\Version\VersionComparator;
     use ncc\Utilities\Functions;
 
     class PackageEntry
@@ -15,6 +17,13 @@
          * @var string
          */
         public $Name;
+
+        /**
+         * The latest version of the package entry, this is updated automatically
+         *
+         * @var string|null
+         */
+        private $LatestVersion;
 
         /**
          * An array of installed versions for this package
@@ -56,6 +65,119 @@
         }
 
         /**
+         * Removes version entry from the package
+         *
+         * @param string $version
+         * @return bool
+         * @noinspection PhpUnused
+         */
+        public function removeVersion(string $version): bool
+        {
+            $count = 0;
+            $found_node = false;
+            foreach($this->Versions as $versionEntry)
+            {
+                if($versionEntry->Version == $version)
+                {
+                    $found_node = true;
+                    break;
+                }
+
+                $count += 1;
+            }
+
+            if($found_node)
+            {
+                unset($this->Versions[$count]);
+                $this->updateLatestVersion();
+                return true;
+            }
+
+            return false;
+        }
+
+        /**
+         * Adds a new version entry to the package, if overwrite is true then
+         * the entry will be overwritten if it exists, otherwise it will return
+         * false.
+         *
+         * @param Package $package
+         * @param bool $overwrite
+         * @return bool
+         */
+        public function addVersion(Package $package, bool $overwrite=false): bool
+        {
+            try
+            {
+                if ($this->getVersion($package->Assembly->Version) !== null)
+                {
+                    if (!$overwrite) return false;
+                    $this->removeVersion($package->Assembly->Version);
+                }
+            }
+            catch (VersionNotFoundException $e)
+            {
+                unset($e);
+            }
+
+            $version = new VersionEntry();
+            $version->Version = $package->Assembly->Version;
+            $version->Compiler = $package->Header->CompilerExtension;
+            $version->ExecutionUnits = $package->ExecutionUnits;
+
+            foreach($package->Dependencies as $dependency)
+            {
+                $version->Dependencies[] = new DependencyEntry($dependency);
+            }
+
+            $this->Versions[] = $version;
+            $this->updateLatestVersion();
+            return true;
+        }
+
+        /**
+         * Updates and returns the latest version of this package entry
+         *
+         * @return void
+         */
+        private function updateLatestVersion(): void
+        {
+            $latest_version = null;
+            foreach($this->Versions as $version)
+            {
+                if(VersionComparator::compareVersion($version, $latest_version))
+                    $latest_version = $version;
+            }
+
+            $this->LatestVersion = $latest_version;
+        }
+
+        /**
+         * @return string|null
+         */
+        public function getLatestVersion(): ?string
+        {
+            return $this->LatestVersion;
+        }
+
+        /**
+         * Returns an array of all versions installed
+         *
+         * @return array
+         */
+        public function getVersions(): array
+        {
+            $r = [];
+
+            foreach($this->Versions as $version)
+            {
+                $r[] = $version->Version;
+            }
+
+            return $r;
+        }
+
+        /**
          * Returns an array representation of the object
          *
          * @param bool $bytecode
@@ -71,6 +193,7 @@
 
             return [
                 ($bytecode ? Functions::cbc('name')  : 'name')  => $this->Name,
+                ($bytecode ? Functions::cbc('latest_version')  : 'latest_version')  => $this->LatestVersion,
                 ($bytecode ? Functions::cbc('versions')  : 'versions')  => $versions,
             ];
         }
@@ -86,6 +209,7 @@
             $object = new self();
 
             $object->Name = Functions::array_bc($data, 'name');
+            $object->LatestVersion = Functions::array_bc($data, 'latest_version');
             $versions = Functions::array_bc($data, 'versions');
 
             if($versions !== null)
@@ -98,4 +222,5 @@
 
             return $object;
         }
+
     }
