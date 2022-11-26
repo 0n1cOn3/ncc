c90ba6d9c1fd4ea68a5e4769bd01ecba9f6157db
Netkas <zixing.narrakas1996@gmail.com>
2022-11-25 21:26:07 -0500

    Implemented save() and load() method to \ncc\Managers > ConfigurationManager

--- src/ncc/Managers/ConfigurationManager.php
+++ src/ncc/Managers/ConfigurationManager.php
@@ -1,16 +1,74 @@
 <?php
 
+    /** @noinspection PhpMissingFieldTypeInspection */
+
     namespace ncc\Managers;
+
+    use ncc\Abstracts\Scopes;
+    use ncc\Exceptions\AccessDeniedException;
+    use ncc\Exceptions\FileNotFoundException;
+    use ncc\Exceptions\InvalidScopeException;
+    use ncc\Exceptions\IOException;
+    use ncc\ThirdParty\Symfony\Yaml\Yaml;
+    use ncc\Utilities\IO;
+    use ncc\Utilities\PathFinder;
+    use ncc\Utilities\Resolver;
+    use ncc\Utilities\RuntimeCache;
 
     class ConfigurationManager
     {
         /**
-         * Public Constructor
+         * The configuration contents parsed
          *
-         * @param string $package
+         * @var mixed
          */
-        public function __construct(string $package)
+        private $Configuration;
+
+        /**
+         * @throws AccessDeniedException
+         * @throws FileNotFoundException
+         * @throws IOException
+         * @throws InvalidScopeException
+         */
+        public function __construct()
         {
-            $this->Package = $package;
+            $this->load();
+        }
+
+        /**
+         * Loads the configuration file if it exists
+         *
+         * @return void
+         * @throws AccessDeniedException
+         * @throws FileNotFoundException
+         * @throws IOException
+         * @throws InvalidScopeException
+         */
+        public function load()
+        {
+            $this->Configuration = RuntimeCache::get('ncc.yaml');
+            if($this->Configuration !== null)
+                return;
+            $configuration_contents = IO::fread(PathFinder::getConfigurationFile());
+            $this->Configuration = Yaml::parse($configuration_contents);
+        }
+
+        /**
+         * Saves the configuration file to disk
+         *
+         * @return void
+         * @throws AccessDeniedException
+         * @throws IOException
+         * @throws InvalidScopeException
+         */
+        public function save()
+        {
+            if(Resolver::resolveScope() !== Scopes::System)
+                throw new AccessDeniedException('Cannot save configuration file, insufficient permissions');
+
+            if($this->Configuration == null)
+                return;
+
+            IO::fwrite(PathFinder::getConfigurationFile(), Yaml::dump($this->Configuration));
         }
     }
