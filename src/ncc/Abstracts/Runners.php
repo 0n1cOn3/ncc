f4abc020ec409c86e55d61918a3d2f9f07d3884e
Netkas <zixing.narrakas1996@gmail.com>
2022-12-07 20:59:34 -0500

    Added more Runners to \ncc\Abstracts > Runners
    https://git.n64.cc/nosial/ncc/-/issues/32

--- src/ncc/Abstracts/Runners.php
+++ src/ncc/Abstracts/Runners.php
@@ -5,4 +5,30 @@
     abstract class Runners
     {
         const php = 'php';
+
+        const bash = 'bash';
+
+        const sh = 'sh';
+
+        const python = 'python';
+
+        const python3 = 'python3';
+
+        const python2 = 'python2';
+
+        const perl = 'perl';
+
+        const lua = 'lua';
+
+
+        const All = [
+            self::php,
+            self::bash,
+            self::sh,
+            self::python,
+            self::python3,
+            self::python2,
+            self::perl,
+            self::lua
+        ];
     }
