daa954bd7623bcc430fede79560b56ebb3d4ccef
Netkas <zixing.narrakas1996@gmail.com>
2022-12-07 20:59:55 -0500

    Updated build flag

--- src/ncc/Abstracts/NccBuildFlags.php
+++ src/ncc/Abstracts/NccBuildFlags.php
@@ -8,5 +8,5 @@
          * Indicates if the build is currently unstable and some features may not work correctly
          * and can cause errors
          */
-        const Unstable = 'UNSTABLE';
+        const Unstable = 'unstable';
     }
