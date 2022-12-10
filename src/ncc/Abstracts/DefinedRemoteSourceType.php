b3c877931396146e42cdee2ba6c77f85ec0a85ac
Netkas <zixing.narrakas1996@gmail.com>
2022-12-10 04:29:17 -0500

    Added DefinedRemoteSourceType.php
    https://git.n64.cc/nosial/ncc/-/issues/28

---
+++ src/ncc/Abstracts/DefinedRemoteSourceType.php
@@ -0,0 +1,12 @@
+<?php
+
+    namespace ncc\Abstracts;
+
+    abstract class DefinedRemoteSourceType
+    {
+        const Git = 'git';
+
+        const Gitlab = 'gitlab';
+
+        const Github = 'github';
+    }
