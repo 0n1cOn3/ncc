b4acfaa0cb866a726595bed8639e2ff8077067f4
Netkas <zixing.narrakas1996@gmail.com>
2022-12-15 01:27:58 -0500

    Implemented \ncc\Objects\ProjectConfiguration > updateSource
    https://git.n64.cc/nosial/ncc/-/issues/34

--- src/ncc/Objects/ProjectConfiguration/UpdateSource.php
+++ src/ncc/Objects/ProjectConfiguration/UpdateSource.php
@@ -1,8 +1,58 @@
 <?php
 
+    /** @noinspection PhpMissingFieldTypeInspection */
+
     namespace ncc\Objects\ProjectConfiguration;
+
+    use ncc\Objects\ProjectConfiguration\UpdateSource\Repository;
+    use ncc\Utilities\Functions;
 
     class UpdateSource
     {
-        public $type;
+        /**
+         * The string format of where the source is located.
+         *
+         * @var string
+         */
+        public $Source;
+
+        /**
+         * The repository to use for the source
+         *
+         * @var Repository|null
+         */
+        public $Repository;
+
+        /**
+         * Returns an array representation of the object
+         *
+         * @param bool $bytecode
+         * @return array
+         */
+        public function toArray(bool $bytecode=false): array
+        {
+            return [
+                ($bytecode ? Functions::cbc('source') : 'source') => $this->Source,
+                ($bytecode ? Functions::cbc('repository') : 'repository') => ($this->Repository?->toArray($bytecode))
+            ];
+        }
+
+
+        /**
+         * Constructs object from an array representation
+         *
+         * @param array $data
+         * @return UpdateSource
+         */
+        public static function fromArray(array $data): UpdateSource
+        {
+            $obj = new UpdateSource();
+            $obj->Source = Functions::array_bc($data, 'source');
+            $obj->Repository = Functions::array_bc($data, 'repository');
+
+            if($obj->Repository !== null)
+                $obj->Repository = Repository::fromArray($obj->Repository);
+
+            return $obj;
+        }
     }
