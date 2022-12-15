d2372185629a1c2722faf0f40e838ec05b656032
Netkas <zixing.narrakas1996@gmail.com>
2022-12-15 01:05:53 -0500

    Added \ncc\Abstracts > RuntimeImportOptions
    https://git.n64.cc/nosial/ncc/-/issues/33

---
+++ src/ncc/Abstracts/RuntimeImportOptions.php
@@ -0,0 +1,18 @@
+<?php
+
+    namespace ncc\Abstracts;
+
+    abstract class RuntimeImportOptions
+    {
+        /**
+         * Indicates if the import should require PHP's autoload.php file
+         * for the package (Only applies to PHP packages)
+         */
+        const ImportAutoloader = 'import_autoloader';
+
+        /**
+         * Indicates if the import should require all static files
+         * for the package (Only applies to PHP packages)
+         */
+        const ImportStaticFiles = 'import_static_files';
+    }
