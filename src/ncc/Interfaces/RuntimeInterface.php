b9252a0fda9a64d9fc957543d5f3165bb9fdb966
Netkas <zixing.narrakas1996@gmail.com>
2022-12-15 01:06:43 -0500

    Added \ncc\Interfaces > RuntimeInterface
    https://git.n64.cc/nosial/ncc/-/issues/33

---
+++ src/ncc/Interfaces/RuntimeInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+    namespace ncc\Interfaces;
+
+    use ncc\Abstracts\Versions;
+    use ncc\Exceptions\MissingDependencyException;
+    use ncc\Exceptions\PackageLockException;
+    use ncc\Exceptions\PackageNotFoundException;
+    use ncc\Exceptions\VersionNotFoundException;
+    use ncc\Objects\PackageLock\VersionEntry;
+
+    interface RuntimeInterface
+    {
+        /**
+         * @param VersionEntry $versionEntry
+         * @param array $options
+         * @return mixed
+         * @throws PackageNotFoundException
+         * @throws VersionNotFoundException
+         * @throws PackageLockException
+         * @throws MissingDependencyException
+         */
+        public static function import(VersionEntry $versionEntry, array $options=[]): bool;
+    }
