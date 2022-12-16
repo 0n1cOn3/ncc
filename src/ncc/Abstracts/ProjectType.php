6a37c8216d4e789b94d76d9051648e87cfef33c3
Netkas <zixing.narrakas1996@gmail.com>
2022-12-16 00:37:06 -0500

    Added ProjectType to \ncc\Abstracts

---
+++ src/ncc/Abstracts/ProjectType.php
@@ -0,0 +1,12 @@
+<?php
+
+    namespace ncc\Abstracts;
+
+    abstract class ProjectType
+    {
+        const Composer = 'composer';
+
+        const Ncc = 'ncc';
+
+        const Unknown = 'unknown';
+    }
