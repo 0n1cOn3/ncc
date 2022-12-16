d154aa02ca1bc45b5b9071a8e14c1961a58cfeae
Netkas <zixing.narrakas1996@gmail.com>
2022-12-15 21:19:33 -0500

    Updated \ncc\Abstracts > DefinedRemoteSourceType
    https://git.n64.cc/nosial/ncc/-/issues/28

--- src/ncc/Abstracts/DefinedRemoteSourceType.php
+++ src/ncc/Abstracts/DefinedRemoteSourceType.php
@@ -5,20 +5,6 @@
     abstract class DefinedRemoteSourceType
     {
         /**
-         * The remote source is from a generic remote git server
-         * (Will search for packages with /group/package)
-         *
-         * For example if the host is git.example.com and the package is
-         * group/package, the package will be fetched from
-         * https://git.example.com/group/package.git
-         *
-         * The git client will be used to fetch the package
-         * but NCC will not be able to easily check for updates
-         * without having to pull the entire repository
-         */
-        const Git = 'git';
-
-        /**
          * THe remote source is from gitlab or a custom gitlab instance
          *
          * Will use an API wrapper to interact with the gitlab instance
@@ -41,7 +27,6 @@
         const Github = 'github';
 
         const All = [
-            self::Git,
             self::Gitlab,
             self::Github
         ];
